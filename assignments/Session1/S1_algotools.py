223a26ad2ea9e69b9795926c565c3530d3690733
benjaminlry <benjamin.leroy73100@gmail.com>
2018-10-08 15:28:42 +0200

    Exercice 2 (max value) done

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -55,10 +55,69 @@ def average_above_zero(tab):
 
     
 #Test script for average_above_zero
-test_tab=[1,2,3,-5, 100]
-moy=average_above_zero(test_tab)
+test_tab_average=[1,2,3,-5]
+moy=average_above_zero(test_tab_average)
 #print('Positive values average = '+str(moy))
 print('Positive values average = {v}'.format(v=moy))
+
+
+def max_value_perso(tab): 
+    """
+    Brief: computes the max with positiv value
+    Args: 
+        tab: a liste of numeric value exepcts at least one positive valus, raise expection
+    Return: the max value
+    Raises :
+        ValueError if no positive value is found
+    """
+    if not(isinstance(tab, list)):
+        raise ValueError('Expected a list as input')
+    
+    numberMaxi=0
+        
+    for val in tab:
+        if val > 0:
+            if val > numberMaxi:
+                numberMaxi=val
+    return numberMaxi
+
+    
+#Test script for average_above_zero
+test_tab_max=[1,2,3,-5, 100]
+maxPerso=max_value_perso(test_tab_max)
+#print('Positive values average = '+str(moy))
+print('Positive values max = {v}'.format(v=maxPerso))
+
+
+
+"""
+def average_above_zero(input_list):
+    ##
+    # compute the average of positive values
+    # @input_list : the list of values to process
+    # @return the average value of all the positive elements
+
+    #init critical variable
+    positive_values_sum=0
+    positive_values_count=0
+
+    first_item=input_list[0] #just a line to generate a code smell with an unused value
+
+    #compute the average of positive elements of a list
+    for item in input_list:
+        #select only positive items
+        if item>0:
+            positive_values_sum+=item
+            positive_values_count+=1
+        elif item==0:
+            print('This value is null:'+str(item))
+        else:
+            print('This value is negative:'+str(item))
+    #compute the final average
+    average=float(positive_values_sum)/float(positive_values_count)
+    print('Positive elements average is '+str(average))
+    return float(average)
+"""
 
 """#testing average_above_zero function:
 mylist=[1,2,3,4,-7]
@@ -103,68 +162,4 @@ def max_value(input_list):
             max_idx=idx
 
     return max_val, max_idx
-    
-"""
-
-
-#test max_value function
-#1 basic test, expected answer=2
-mylist=[-1,2,-20]
-"""mymax, mymaxidx=max_value(mylist)
-mymax_tuple=max_value(mylist)
-mymax=mymax_tuple[0]"""
-print('Max value of {input_list} is {max_scan}'.format(input_list=mylist, max_scan=mymax))
-#==> message : "Max value of [-1, 2, -20] is (2, 1)"
-
-#2 error test : Exception expected
-max_value([])
-
-
-"""
-# hints to solve the roi_bbox function exercise: numpy basics
-
-#matrix processing lib
-import numpy
-
-#create a numpy matrix with specific dimensions
-size_rows=10
-size_cols=10
-myMat=numpy.zeros([size_rows, size_cols], dtype=int)
-#set a value in a specific cell
-myMat[1,3]=1
-
-#fil something in the matrix, the basic way (a very slow python way...)
-for row in range(5,8):
-    for col in range(7,9):
-        myMat[row,col]=1
-
-#get time to measure processing speed
-import time
-init_time=time.time()
-
-#filling something in the matrix, a nicer way
-myMat[2:4,5:9]=1 #assign a scalar to each cell of a subarray
-myMat[4:7,7:9]=numpy.ones([3,2]) #copy an array in a subarray
-print(myMat)
-
-#get ellapsed time
-filling_time=time.time() -init_time
-print('data prefecting time='+str(filling_time))
-
-#fake bounding box coordinates matrix
-xmin=0
-xmax=100
-ymin=0
-ymax=200
-#how to fill the 4x2 bbox coordinates matrix, method 1 using 1D numpy arrays (ugly?)
-bbox_coords=numpy.zeros([4, 2], dtype=int)
-bbox_coords[0,:]=numpy.array([ymin, xmin])
-bbox_coords[1,:]=numpy.array([ymin, xmax])
-bbox_coords[2,:]=numpy.array([ymax, xmin])
-bbox_coords[3,:]=numpy.array([ymax, xmax])
-#how to fill the 4x2 bbox coordinates matrix, method 2 using lists (shorter way)
-#->create a list of lists
-coordsList=[[ymin, xmin],[ymin, xmax],[ymax, xmin],[ymax, xmax]]
-#->convert to an array
-coords_array=numpy.array(coordsList)
-"""
+    """
